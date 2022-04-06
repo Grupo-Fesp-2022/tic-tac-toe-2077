4ce081fa457e05e706ad10a96b3efbe27a104848
Rafael <rafael271alves@gmail.com>
2022-04-06 10:52:00 -0300

    criação da documentação de funções uteis

--- src/utils/system.c
+++ src/utils/system.c
@@ -1,8 +1,15 @@
+/**
+ * @brief Limpa a tela do console
+ */
 void clear()
 {
     system("cls");
 }
 
+/**
+ * @brief Deixa o console ocupado, com a mensagem
+ * "Pressione qualquer tecla para continuar ..."
+ */
 void pause()
 {
     system("pause");
