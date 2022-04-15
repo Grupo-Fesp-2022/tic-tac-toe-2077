d1d6daf5bc2ec60500806297d050aafa64c6708b
ohardu <82791647+ohardu@users.noreply.github.com>
2022-04-14 21:11:36 -0300

    remoção de código redundante

--- src/pages/main.c
+++ src/pages/main.c
@@ -8,11 +8,6 @@ void star_game()
     // linkando o array de pontuação no ponteiro global
     SCORES = calloc(3, sizeof(int));
 
-    // Iniciando as pontuações
-    SCORES[T_X] = 0;
-    SCORES[T_O] = 0;
-    SCORES[T_C] = 0;
-
     // Iniciando o jogo
     start_menu();
 
