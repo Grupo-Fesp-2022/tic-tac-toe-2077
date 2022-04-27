d54b1e6e5eb955baf722b0adcb33bf765a51ac85
Rafael <rafael271alves@gmail.com>
2022-04-27 17:32:44 -0300

    app roda em modo dark

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gcc -Wl,--export-all-symbols ../src/main.c -o ../build/main.exe `pkg-config --cflags gtk+-3.0` `pkg-config --libs gtk+-3.0`
-../build/main.exe
+GTK_THEME=Adwaita:dark ../build/main.exe
