93434909bf558906e2335e31bf5e519d94f20a9c
Rafael <rafael271alves@gmail.com>
2022-04-28 08:38:31 -0300

    ajuste no script de build

--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-gcc -Wl,--export-all-symbols ../src/main.c -o ../build/main.exe `pkg-config --cflags gtk+-3.0` `pkg-config --libs gtk+-3.0`
-GTK_THEME=Adwaita:dark ../build/main.exe
+cp ../src/ui/*.glade ../build/
+gcc -Wl,--export-all-symbols ../src/main.c -o ../build/main.exe $(pkg-config --cflags gtk+-3.0) $(pkg-config --libs gtk+-3.0)
+./run.sh
