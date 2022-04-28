04fea6c9c13a8b2d938e02a076342894523799d5
Rafael <rafael271alves@gmail.com>
2022-04-28 08:39:39 -0300

    atualização do caminho do glade

--- src/elements/main.c
+++ src/elements/main.c
@@ -3,12 +3,8 @@
 void init_widgets()
 {
     // iniciando o builder
-    // OBS: a pasta do .glade precisa ser informada, saindo da pasta build
-    builder = gtk_builder_new_from_file("../src/ui/ui.glade");
+    builder = gtk_builder_new_from_file("../build/ui.glade");
 
     // linkando os widgets da tela
-    fixed1 = GTK_WIDGET(gtk_builder_get_object(builder, "fixed1"));
-    btn1 = GTK_WIDGET(gtk_builder_get_object(builder, "btn1"));
-    lbl1 = GTK_WIDGET(gtk_builder_get_object(builder, "lbl1"));
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
 }
