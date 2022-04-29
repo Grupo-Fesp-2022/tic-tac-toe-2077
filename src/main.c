980d2ffd6a87ccc1bd8e9a58ab939d187c78de90
Rafael <rafael271alves@gmail.com>
2022-04-29 10:08:17 -0300

    trocando o widget_show pelo widget_show_all

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <gtk/gtk.h>
 
 #include "./elements/main.c"
+#include "./utils/main.c"
 #include "./events/main.c"
 
 int main(int argc, char **argv)
@@ -13,7 +14,7 @@ int main(int argc, char **argv)
     gtk_builder_connect_signals(builder, NULL);
 
     // mostrando a tela e iniciando o loop principal
-    gtk_widget_show(window);
+    gtk_widget_show_all(window);
     gtk_main();
 
     return EXIT_SUCCESS;
