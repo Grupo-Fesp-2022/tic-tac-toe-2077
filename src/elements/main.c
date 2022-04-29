9fb3d41b4a3d2605b49ecf96096ea52f76e131ce
Rafael <rafael271alves@gmail.com>
2022-04-29 10:07:06 -0300

    criação de outros elementos

--- src/elements/main.c
+++ src/elements/main.c
@@ -1,3 +1,4 @@
+#include "./vars.c"
 #include "./widgets.c"
 
 void init_widgets()
@@ -6,5 +7,11 @@ void init_widgets()
     builder = gtk_builder_new_from_file("../build/ui.glade");
 
     // linkando os widgets da tela
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+    credits_page = GTK_WIDGET(gtk_builder_get_object(builder, "credits_page"));
+    game_single_page = GTK_WIDGET(gtk_builder_get_object(builder, "game_single_page"));
+    lbl_single = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_single"));
+    menu_page = GTK_WIDGET(gtk_builder_get_object(builder, "menu_page"));
+    new_game_page = GTK_WIDGET(gtk_builder_get_object(builder, "new_game_page"));
+    stack = GTK_STACK(gtk_builder_get_object(builder, "stack"));
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
 }
--- src/elements/widgets.c
+++ src/elements/widgets.c
@@ -1,3 +1,8 @@
-GtkWidget *window;
 GtkBuilder *builder;
-
+GtkStack *stack;
+GtkWidget *credits_page;
+GtkWidget *game_single_page;
+GtkWidget *lbl_single;
+GtkWidget *menu_page;
+GtkWidget *new_game_page;
+GtkWidget *window;
